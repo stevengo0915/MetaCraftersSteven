fd4ee34c794087ace0747836a08a936915610efd
Steven Go <94173751+stevengo0915@users.noreply.github.com>
2024-07-10 16:16:09 +0800

    Update Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -1,58 +1,73 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
-
-//import "hardhat/console.sol";
 
 contract Assessment {
     address payable public owner;
     uint256 public balance;
 
-    event Deposit(uint256 amount);
-    event Withdraw(uint256 amount);
+    struct Transaction {
+        address sender;
+        string transactionType;
+        uint256 amount;
+        uint256 timestamp;
+    }
 
-    constructor(uint initBalance) payable {
+    Transaction[] public transactions;
+
+    event Deposit(address indexed sender, uint256 amount);
+    event Withdraw(address indexed sender, uint256 amount);
+
+    constructor(uint256 initBalance) payable {
         owner = payable(msg.sender);
         balance = initBalance;
     }
 
+    receive() external payable {}
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can call this function");
+        _;
+    }
+
     function getBalance() public view returns (uint256) {
-        return balance;
+        return address(this).balance;
     }
 
-    function deposit(uint256 _amount) public payable {
-        uint256 _previousBalance = balance;
-
-        // make sure this is the owner
-        require(msg.sender == owner, "You are not the owner of this account");
-
-        // perform transaction
-        balance += _amount;
-
-        // assert transaction completed successfully
-        assert(balance == _previousBalance + _amount);
-
-        // emit the event
-        emit Deposit(_amount);
+    function deposit() public payable onlyOwner {
+        require(msg.value > 0, "Deposit amount must be greater than 0");
+        
+        balance += msg.value;
+        emit Deposit(msg.sender, msg.value);
+        addTransaction(msg.sender, "Deposit", msg.value);
     }
 
-    // custom error
-    error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
+    function withdraw(uint256 _withdrawAmount) public onlyOwner {
+        require(_withdrawAmount > 0, "Withdrawal amount must be greater than 0");
+        require(address(this).balance >= _withdrawAmount, "Balance is insufficient");
 
-    function withdraw(uint256 _withdrawAmount) public {
-        require(msg.sender == owner, "You are not the owner of this account");
-        uint256 _previousBalance = balance;
-        if (balance < _withdrawAmount) {
-            revert InsufficientBalance({balance: balance, withdrawAmount: _withdrawAmount});
-        }
-
-        // withdraw the given amount
         balance -= _withdrawAmount;
-
-        // assert the balance is correct
-        assert(balance == (_previousBalance - _withdrawAmount));
-
-        // emit the event
-        emit Withdraw(_withdrawAmount);
+        payable(msg.sender).transfer(_withdrawAmount);
+        emit Withdraw(msg.sender, _withdrawAmount);
+        addTransaction(msg.sender, "Withdrawal", _withdrawAmount);
     }
 
+    function addTransaction(address _sender, string memory _transactionType, uint256 _amount) internal {
+        Transaction memory newTransaction = Transaction({
+            sender: _sender,
+            transactionType: _transactionType,
+            amount: _amount,
+            timestamp: block.timestamp
+        });
+        transactions.push(newTransaction);
+    }
+
+    function getTransactionCount() public view returns (uint256) {
+        return transactions.length;
+    }
+
+    function getTransaction(uint256 index) public view returns (address sender, string memory transactionType, uint256 amount, uint256 timestamp) {
+        require(index < transactions.length, "Index out of bounds");
+        Transaction memory transaction = transactions[index];
+        return (transaction.sender, transaction.transactionType, transaction.amount, transaction.timestamp);
+    }
 }
